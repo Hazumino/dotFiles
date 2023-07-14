1cd9d37e234c4967efd6c25c8becb4508be8af15
Hazumino <zenolino@gmail.com>
2023-07-14 16:56:39 +0200

    Symlink transfer

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,19 @@
 #! /bin/sh
 
+ln -s ~/dotFiles/.tmux.conf ~/.tmux.conf
+
 ln -s ~/dotFiles/nvim ~/.config/nvim
+
+ln -s ~/dotFiles/fuzzel ~/.config/fuzzel
+
+ln -s ~/dotFiles/vifm ~/.config/vifm
+
+ln -s ~/dotFiles/kitty ~/.config/kitty
+
+ln -s ~/dotFiles/hypr ~/.config/hypr
+
+ln -s ~/dotFiles/waybar ~/.config/waybar
+
+ln -s ~/dotFiles/rofi ~/.config/rofi
+
+ln -s ~/dotFiles/rofi.zeno ~/.config/rofi.zeno
